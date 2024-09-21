755fbfd4da48594805fa3f31bdb6a3c307b55e95
abundiko <abundiko111@gmail.com>
2024-09-21 17:11:59 +0100

    fixed type issues, cleaned up and prep for first hosting

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: "Bad Decisions Developers: We build cool shit",
   description: "A passionate team of developers building innovative and cutting-edge web applications.",
   openGraph: {
-    images: "https://"
+    images: "https://bad-dd.vercel.app/images/banner.png"
   }
 };
 
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <>
-        {children}
-      </>
+      {children}
+    </>
   );
 }
