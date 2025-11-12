356be5f79d33f2494d2291ba015704e0cd4f6a77
abundiko <abundiko111@gmail.com>
2025-11-12 10:02:24 +0100

    updated sitemap to fix index page url

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 export default async function sitemap() {
   return [
     {
-      url: "/",
+      url: "https://abundiko.vercel.app",
       priority: 1.0,
       changeFrequency: "monthly",
       lastModified: "2025-11-12T08:58:02.494Z",
