d36c673ce149b19408e222b5d62373ad2775a435
abundiko <abundiko111@gmail.com>
2025-11-12 09:58:26 +0100

    added sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default async function sitemap() {
+  return [
+    {
+      url: "/",
+      priority: 1.0,
+      changeFrequency: "monthly",
+      lastModified: "2025-11-12T08:58:02.494Z",
+    },
+  ];
+}
